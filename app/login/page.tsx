37862ede74a61abdaa7ed304b8b198ce249dc740
murashko12 <petr@ocumare.ru>
2026-04-09 10:01:57 +0300

    feat: add protect proxy for pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,15 @@
+import { auth } from "@/auth"
 import { LoginForm } from "@/components/login-form"
+import { redirect } from "next/navigation"
 
-export default function Page() {
+export default async function Page() {
+  
+  const session = await auth()
+  
+  if (session?.user) {
+    redirect('/dashboard')
+  }
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
